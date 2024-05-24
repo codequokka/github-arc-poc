ba7de9a31ea5d748aeea1d242386fe6dba1c5847
Nobuchika Tanaka <codequokka@gmail.com>
2024-05-24 22:01:44 +0000

    Add intented error

--- script/tests/test_http_bin_client.py
+++ script/tests/test_http_bin_client.py
@@ -12,13 +12,13 @@ def mock_http_bin_client() -> Mock:
 
 @patch("mymodule.lib.http_bin_client.HttpBinClient.get_ip")
 def test_get_ip_success(mock_http_bin_client: Mock) -> None:
-    want = {"origin": "10.0.0.1"}
+    want = {"origin": "127.0.0.1"}
     mock_http_bin_client.return_value = want
 
     client = HttpBinClient()
     got = client.get_ip()
 
-    assert got == want
+    assert got != want
 
 
 @patch("mymodule.lib.http_bin_client.HttpBinClient.get_ip")
